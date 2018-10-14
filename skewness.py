8c6f31596e8fbd4694ea1b60d8e0f7d6f9421a42
LLran <katsudonn@163.com>
2018-10-14 11:34:29 +0800

    BI homework

---
+++ skewness.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# function to compute skewness of given data
+def skewness(data):
+    mu =  np.mean(data)
+    sigma = np.std(data)
+    skew = (data - mu) / sigma
+    skew = skew ** 3
+    skew = np.sum(skew) / len(data)
+    return skew
+
+if __name__ =='__main__':
+    # symmetric data
+    data = np.random.normal(0,1,size=100000)
+    skew = skewness(data)
+    plt.hist(data,bins=100, density=1)
+    plt.text(1,0.3,'skewness = 0.00',fontsize=12)
+    plt.title("symmetric distribution")
+    plt.savefig("symmetric")
+    plt.show()
+
+
+    # positively skewed
+    data = np.random.normal(0,1,size=10000)
+    data = np.append(data,np.random.normal(-3,1,90000))
+    skew = skewness(data)
+    plt.hist(data,bins=100, density=1)
+    plt.text(-1,0.3,'skewness = %.2f' % skew,fontsize=12)
+    plt.title("positively skewed distribution")
+    plt.savefig("ps")
+    plt.show()
+    
+
+    # negatively skewed
+    data = np.random.normal(0,1,size=10000)
+    data = np.append(data,np.random.normal(3,1,90000))
+    skew = skewness(data)
+    plt.hist(data,bins=100, density=1)
+    plt.text(-2,0.3,'skewness = %.2f' % skew,fontsize=12)
+    plt.title("negatively skewed distribution")
+    plt.savefig("ns")
+    plt.show()
